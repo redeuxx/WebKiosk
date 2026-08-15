88fa1421e546961998fec47679a0578c0f6782fb
Vernon Wenberg <vernon@wenberg.net>
2026-08-15 01:04:23 -0500

    Initial Commit

---
+++ BrowserHawk/BrowserHawkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserHawkApp.swift
+//  BrowserHawk
+//
+//  Created by Vernon Wenberg on 8/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BrowserHawkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrowserHawk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrowserHawk
+//
+//  Created by Vernon Wenberg on 8/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
